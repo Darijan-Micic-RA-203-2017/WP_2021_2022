819cdaff507e45947b83064504a091a52492b420
Darijan Mićić <darijan.micic10@gmail.com>
2022-06-20 04:33:17 +0200

    fix: remove character from pattern in backend

--- FitPass/src/validators/UserValidator.java
+++ FitPass/src/validators/UserValidator.java
@@ -202,7 +202,7 @@ public class UserValidator {
 		}
 		
 		Pattern pattern = Pattern.compile("(19|20)[0-9]{2}-" + 
-				"(0[1-9]|1[012])-^(0[1-9]|[12][0-9]|3[01])T" + 
+				"(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T" + 
 				"(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])");
 		Matcher matcher = pattern.matcher(dateOfBirth);
 		if (!matcher.matches()) {
